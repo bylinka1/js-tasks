4b0dfbca9952399a7974d960b46cc22c3c51a036
Tatyana <tanya.bylinskaya@inbox.ru>
2022-04-04 20:49:48 +0300

    Changing2 functions-task3

--- functions_task3.js
+++ functions_task3.js
@@ -1,49 +1,66 @@
-var words = [
-            "javascript",
-            "cake",
-            "juice",
-            "jupiter",
-            "bullshit",
-            "sandwich",
-            "butterfly",
-            "sausages"
-        ];
-var pickRandomWord = function(words) {
+var pickWord = function(words) {
+    var words = [
+        "javascript",
+        "cake",
+        "juice",
+        "jupiter",
+        "bullshit",
+        "sandwich",
+        "butterfly",
+        "sausages"
+    ];
     return words[Math.floor(Math.random() * words.length)];
 };
-var word = pickRandomWord(words);
-var answerArray = [];
-        for (var i = 0; i < word.length; i++) {
-            answerArray[i] = "_";
-        }
-var guesses = 10;
-var remainingLetters = word.length;
-while (remainingLetters > 0 && guesses > 0 ) {
-    var showPlayerProgress = function (answerArray) {
-        alert(answerArray.join(" "));
-    };
-    showPlayerProgress(answerArray);
-    var guess = function() {
-        var guess = prompt("Guess the letter, or click Cancel stop playing!");
-    };
-    var updateGameState = function (guess, word, answerArray) {
-        if (guess === null) {
-            return;
-        }
-        if (guessNew.length !== 1) {
-            return alert("Please enter a single letter!");
-        } 
-        return guesses--;
-        for (var j = 0; j < word.length; j++) {
-                    if (word[j] === guess && answerArray[j] === "_") {
-                        return answerArray[j] = guess;
-                        remainingLetters--;
-                    }
-        }
-    };
-var showAnswerAndCongratulatePlayer = function (answerArray) {
-    if (guesses > 0) {
-        return alert("Good job! The answer was " + word); 
+
+var setupAnswerArray = function (word) {
+    var answerArray = [];
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
     }
-    return alert("Very bad! the answer was " + word);
-        };
+    return answerArray;
+};
+
+var showPlayerProgress = function (answerArray) {
+    alert(answerArray.join(" "));
+};
+
+var getGuess = function() {
+    var guess = prompt("Guess the letter, or click Cancel stop playing!");
+    return guess;
+};
+
+var updateGameState = function (guess, word, answerArray) {
+    if (guess.length !== 1) {
+        return alert("Please enter a single letter!");
+    } 
+    
+    var count = 0; 
+    for (var j = 0; j < word.length; j++) {
+        if (word[j] === guess && answerArray[j] === "_") {
+            count++;
+            answerArray[j] = word[j];
+        }
+    }
+    return count;
+};
+
+var showAnswerAndCongratulatePlayer = function (answerArray) {
+    return alert("Good job! The answer was " + word); 
+};
+
+var word = pickWord();
+var answerArray = setupAnswerArray(word);
+var remainingLetters = word.length;
+while (remainingLetters > 0) {
+    showPlayerProgress(answerArray);
+    var guess = getGuess();
+    if (guess === null) {
+        break;
+    } else if (guess.length !== 1) {
+        alert("Please enter a single letter.");
+    } else {
+        var correctGuesses = updateGameState(guess, word, answerArray);
+        remainingLetters -= correctGuesses;
+    }
+}
+showAnswerAndCongratulatePlayer(answerArray);
